279ee30eb4a729aaec765c59e74bd226e6dfb3ed
Trendafil Rozov <t.rozov@gmail.com>
2024-04-12 12:27:37 +0300

    Added postgres lab lecture 1

---
+++ Lecture 1/Lab/04_Add_Constraints.sql
@@ -0,0 +1,8 @@
+ALTER TABLE employees
+ALTER COLUMN salary SET default 0;
+
+ALTER TABLE employees
+ALTER COLUMN salary SET Not NULL;
+
+ALTER TABLE employees
+ALTER COLUMN hiring_date SET Not NULL;
