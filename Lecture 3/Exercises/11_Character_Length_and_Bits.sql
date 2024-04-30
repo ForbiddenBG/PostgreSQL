98799755ee8bc69471d5d0a695c77c34f2357dc1
Trendafil Rozov <t.rozov@gmail.com>
2024-04-30 20:38:53 +0300

    Added task 11

---
+++ Lecture 3/Exercises/11_Character_Length_and_Bits.sql
@@ -0,0 +1,10 @@
+SELECT
+	CONCAT(mou.mountain_range, ' ', pea.peak_name) AS mountain_information,
+	CHAR_LENGTH(CONCAT(mou.mountain_range, ' ', pea.peak_name)) AS characters_length,
+    BIT_LENGTH(CONCAT(mou.mountain_range, ' ', pea.peak_name)) AS bits_of_a_string
+FROM
+	mountains AS mou,
+	peaks AS pea
+
+WHERE
+	mou.id = pea.mountain_id
